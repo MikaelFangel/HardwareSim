542f8d95dd66a6ac9dbf1e99b8ed694096765f4a
Silja Sandersen <silja.sandersen@hotmail.com>
2022-10-01 22:36:33 +0200

    Formatted Environment.java

--- Environment.java
+++ Environment.java
@@ -2,23 +2,26 @@ import java.util.HashMap;
 import java.util.Map.Entry;
 
 class Environment {
-    private HashMap<String,Boolean> variableValues = new HashMap<String,Boolean>();
+    private HashMap<String, Boolean> variableValues = new HashMap<String, Boolean>();
 
     public void setVariable(String name, Boolean value) {
         variableValues.put(name, value);
     }
 
-    public Boolean getVariable(String name){
-        Boolean value = variableValues.get(name); 
-        if (value == null) { System.err.println("Variable not defined: "+name); System.exit(1); }
+    public Boolean getVariable(String name) {
+        Boolean value = variableValues.get(name);
+        if (value == null) {
+            System.err.println("Variable not defined: " + name);
+            System.exit(1);
+        }
         return value;
     }
 
     public String toString() {
         String table = "";
-        for (Entry<String,Boolean> entry : variableValues.entrySet()) {
+        for (Entry<String, Boolean> entry : variableValues.entrySet()) {
             table += entry.getKey() + "\t-> " + entry.getValue() + "\n";
         }
         return table;
-    }   
+    }
 }
