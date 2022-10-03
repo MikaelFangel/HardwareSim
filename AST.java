6c22e2af26b95a51768793c4fac7e0c181aed707
Silja Sandersen <silja.sandersen@hotmail.com>
2022-10-03 21:20:25 +0200

    Update AST.java
    Added functionality to functions (Can't compile)

--- AST.java
+++ AST.java
@@ -8,11 +8,166 @@ import java.util.List;
 
 public abstract class AST {};
 
-// class Prog extends AST {
-//     Hardware hardware;
-//     Input 
-// }
+class Prog extends AST {
+    Hardware hardware;
+    Input input;
+    Output output;
+    Latch latch;
+    Update update;
+    Simulate simulate;
 
+    public Prog(Hardware hardware, Input input, Output output, Latch latch, Update update, Simulate simulate) {
+        this.hardware = hardware;
+        this.input = input;
+        this.output = output;
+        this.latch = latch;
+        this.update = update;
+        this.simulate = simulate;
+    }
+
+    public void eval(Environment env) {
+        hardware.eval(env);;
+        input.eval(env);;
+        output.eval(env);;
+        latch.eval(env);;
+        update.eval(env);;
+        simulate.eval(env);;
+    }
+}
+
+class Hardware extends AST {
+    Variable var;
+
+    public Hardware(Variable var) {
+        this.var = var;
+    }
+
+    public void eval(Environment env) {
+        var.eval(env);
+    }
+}
+
+//-----------------------------------------------------------------------------------
+// Input and Output
+//-----------------------------------------------------------------------------------
+class Input extends AST {
+    VariableList varList;
+
+    Input(VariableList varList) {
+        this.varList = varList;
+    }
+
+    public void eval(Environment env) {
+        varList.eval(env);
+    }
+}
+
+class Output extends AST {
+    VariableList varList;
+
+    Output(VariableList varList) {
+        this.varList = varList;
+    }
+
+    public void eval(Environment env) {
+        varList.eval(env);
+    }
+}
+
+class VariableList extends AST {
+    List<Variable> varList;
+
+    VariableList(List<Variable> varList) {
+        this.varList = varList;
+    }
+
+    public void eval(Environment env) {
+        for (Variable var : varList) {
+            var.eval(env);
+        }
+    }
+}
+
+//-----------------------------------------------------------------------------------
+// Simulate
+//-----------------------------------------------------------------------------------
+class Simulate extends AST {
+    SimIn s;
+
+    public Simulate(SimIn s) {
+        this.s = s;
+    }
+
+    public void eval(Environment env) {
+        s.eval(env);
+    }
+}
+
+abstract class SimIns extends AST {
+    abstract public void eval(Environment env);
+}
+
+class MultiSim extends UpdateDecs {
+    SimIn s1, s2;
+
+    public MultiSim(SimIn s1, SimIn s2) {
+        this.s1 = s1;
+        this.s2 = s2;
+    }
+
+    public void eval(Environment env) {
+        s1.eval(env);
+        s2.eval(env);
+    }
+}
+
+class SingleSim extends UpdateDecs {
+    SimIn s1;
+
+    public SingleSim(SimIn s1) {
+        this.s1 = s1;
+    }
+
+    public void eval(Environment env) {
+        s1.eval(env);
+    }
+}
+
+class SimIn extends AST {
+    Variable var;
+    BinaryList binaryList;
+
+    public SimIn(Variable var, BinaryList binaryList) {
+        this.var = var;
+        this.binaryList = binaryList;
+    }
+
+    public void eval(Environment env) {
+        var.eval(env);
+        binaryList.eval(env);
+    }
+}
+
+// ASK TA. Hashmap with list of binaries allowed? 
+// Assign in for each
+class BinaryList extends AST {
+    List<Binary> binaryList;
+
+    BinaryList(List<Binary> binaryList) {
+        this.binaryList = binaryList;
+    }
+
+    public void eval(Environment env) {
+        for (Binary binary : binaryList) {
+            
+            binary.eval(env);
+        }
+    }
+}
+
+//-----------------------------------------------------------------------------------
+// Latches
+//-----------------------------------------------------------------------------------
 class MultiLatch extends AST {
     Latch l1, l2;
 
@@ -74,26 +229,77 @@ class LatchDec extends AST {
     }
 }
 
-class Update extends AST {
-   Variable v;
-   Expr e;
 
-   public Update(Variable v, Expr e) {
-       this.v = v;
-       this.e = e;
+//-----------------------------------------------------------------------------------
+// Updates
+//-----------------------------------------------------------------------------------
+class Update extends AST {
+    UpdateDecs u;
+
+    public Update(UpdateDecs u) {
+        this.u = u;
     }
-   public Boolean eval(Environment env) {
-       env.setVariable(v.varName, e.eval(env));
-       return false;
+
+    public void eval(Environment env) {
+        u.eval(env);
     }
 }
 
+abstract class UpdateDecs extends AST {
+    abstract public void eval(Environment env);
+}
+
+class MultiUpdate extends UpdateDecs {
+    UpdateDec u1, u2;
+
+    public MultiUpdate(UpdateDec u1, UpdateDec u2) {
+        this.u1 = u1;
+        this.u2 = u2;
+    }
+
+    public void eval(Environment env) {
+        u1.eval(env);
+        u2.eval(env);
+    }
+}
+
+class SingleUpdate extends UpdateDecs {
+    UpdateDec u1;
+
+    public SingleUpdate(UpdateDec u1) {
+        this.u1 = u1;
+    }
+
+    public void eval(Environment env) {
+        u1.eval(env);
+    }
+}
+
+class UpdateDec extends AST {
+   Variable var;
+   Expr e;
+
+   public UpdateDec(Variable var, Expr e) {
+       this.var = var;
+       this.e = e;
+    }
+
+    // Adds to variable to Environment
+   public void eval(Environment env) {
+       env.setVariable(var.varName, e.eval(env));
+    }
+}
+
+//-----------------------------------------------------------------------------------
+// Expressions
+//-----------------------------------------------------------------------------------
 abstract class Expr extends AST {
     abstract public Boolean eval(Environment env);
 }
 
 class Negation extends Expr {
     Expr c1;
+
     Negation(Expr c1) {
         this.c1 = c1;
     }
@@ -105,6 +311,7 @@ class Negation extends Expr {
 
 class Conjunction extends Expr {
     Expr c1, c2;
+
     Conjunction(Expr c1, Expr c2) {
         this.c1 = c1;
         this.c2 = c2;
@@ -142,6 +349,9 @@ class Variable extends Expr {
     }
 }
 
+//-----------------------------------------------------------------------------------
+// Binary
+//-----------------------------------------------------------------------------------
 class Binary extends AST {
     public Boolean binary;
 
