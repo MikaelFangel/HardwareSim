b857c2f0bf60d11f1a38f9fd471b6332bfc825f7
mikael <mikael.fangel@pm.me>
2022-10-05 19:32:48 +0200

    updated variables to use list of bool everywhere

--- Environment.java
+++ Environment.java
@@ -1,15 +1,17 @@
 import java.util.HashMap;
 import java.util.Map.Entry;
+import java.util.ArrayList;
+import java.util.List;
 
 class Environment {
-    private HashMap<String, Boolean> variableValues = new HashMap<String, Boolean>();
+    private HashMap<String, List<Boolean>> variableValues = new HashMap<>();
 
-    public void setVariable(String name, Boolean value) {
+    public void setVariable(String name, List<Boolean> value) {
         variableValues.put(name, value);
     }
 
-    public Boolean getVariable(String name) {
-        Boolean value = variableValues.get(name);
+    public List<Boolean> getVariable(String name) {
+        List<Boolean> value = variableValues.get(name);
         if (value == null) {
             System.err.println("Variable not defined: " + name);
             System.exit(1);
@@ -19,8 +21,8 @@ class Environment {
 
     public String toString() {
         String table = "";
-        for (Entry<String, Boolean> entry : variableValues.entrySet()) {
-            table += entry.getKey() + "\t-> " + entry.getValue() + "\n";
+        for (Entry<String, List<Boolean>> entry : variableValues.entrySet()) {
+            table += entry.getKey() + "\t-> " + entry.getValue().get(0) + "\n";
         }
         return table;
     }
