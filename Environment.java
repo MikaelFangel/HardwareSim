45c1a0fb53b40bc6a0e40d8f4c2e78202845c130
Mads Sørensen <s215805@student.dtu.dk>
2022-10-07 22:35:49 +0200

    Changed from Binary List to String

--- Environment.java
+++ Environment.java
@@ -4,14 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Environment {
-    private HashMap<String, List<Boolean>> variableValues = new HashMap<>();
+    private HashMap<String, String> variableValues = new HashMap<>();
 
-    public void setVariable(String name, List<Boolean> value) {
+    public void setVariable(String name, String value) {
         variableValues.put(name, value);
     }
 
-    public List<Boolean> getVariable(String name) {
-        List<Boolean> value = variableValues.get(name);
+    public String getVariable(String name) {
+        String value = variableValues.get(name);
         if (value == null) {
             System.err.println("Variable not defined: " + name);
             System.exit(1);
@@ -21,7 +21,7 @@ class Environment {
 
     public String toString() {
         String table = "";
-        for (Entry<String, List<Boolean>> entry : variableValues.entrySet()) {
+        for (Entry<String, String> entry : variableValues.entrySet()) {
             table += entry.getKey() + "\t-> " + entry.getValue() + "\n";
         }
         return table;
